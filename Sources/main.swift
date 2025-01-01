841f90cb4cb00596dcc7206d704ecc970d1c9525
Artix <artixzhang@icloud.com>
2025-01-01 22:08:22 +0800

    03_dictionary_test

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,13 +1,2 @@
-let result = AddTwoNumbers_2.addTwoNumbers(AddTwoNumbers_2.l11, AddTwoNumbers_2.l21)
-
-// 打印结果链表
-func printList(_ head: ListNode?) {
-    var current = head
-    while current != nil {
-        print(current!.val, terminator: " -> ")
-        current = current?.next
-    }
-    print("nil")
-}
-
-printList(result)
+let result = LongestSubstringWithoutRepeatingCharacters.lengthOfLongestSubstring(LongestSubstringWithoutRepeatingCharacters.input)
+print(result)
