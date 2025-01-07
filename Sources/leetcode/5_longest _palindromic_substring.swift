7e36c28d8ae5a4434bfe4d7ee69062b61fa10b38
Artix <artixzhang@icloud.com>
2025-01-07 23:55:53 +0800

    _5

--- Sources/leetcode/5_longest _palindromic_substring.swift
+++ Sources/leetcode/5_longest _palindromic_substring.swift
@@ -1,33 +1,39 @@
 class LongestPalindrome_5{
-    static let string = "cbbd"
+    //两种回文型: abcba abccba
+    static let string = "abcbaa"
     static func longestPalindrome(_ s: String) -> String {
+        if s.count == 1 {
+            return s
+        } else if s.count == 2 {
+            if s.first == s.last {
+                return s
+            } else {
+                return String(s.first!)
+            }
+        }
+        //s有最少3个元素
         let indexStart = s.startIndex
-        var indexCurrent = indexStart
-        var indexExpect = indexCurrent
-        var charExpect: Character? = s[indexStart]
         var lengthMax = 1
         var lengthCurrent = 1
         var stringPalindrome = ""
-        for (offset, _) in s.enumerated().dropFirst() {
-            indexCurrent = s.index(indexStart, offsetBy: offset)
-            print("char:\(s[indexCurrent]), expect:\(charExpect ?? "@")")
-            if s[indexCurrent] == charExpect {
+        if s.first == s.dropFirst().first {
+            lengthMax = 2
+            stringPalindrome = String(s.prefix(2))
+        }
+        var indexPrePre: String.Index? = indexStart
+        var indexPre = s.index(indexStart, offsetBy: 1)
+        var indexCurrent: String.Index
+        //从第[2]个字符开始
+        for i in 2..<s.count {
+            indexCurrent = s.index(indexStart, offsetBy: i)
+            if s[indexCurrent] == s[indexPre] {
                 lengthCurrent += 1
-                if lengthCurrent > lengthMax {
-                    lengthMax = lengthCurrent
-                    stringPalindrome = String(s[indexExpect...indexCurrent])
-                    print(stringPalindrome)
-                }
-                if indexExpect != indexStart {
-                    indexExpect = s.index(indexExpect, offsetBy: -1)
-                    charExpect = s[indexExpect]
+            } else if s[indexCurrent] == s[indexPrePre] {
+                if indexPrePre != indexStart {
+                    indexPrePre = s.index(indexPrePre, offsetBy: -1)
                 } else {
-                    charExpect = nil
+                    indexPrePre = nil
                 }
-            } else {
-                indexExpect = s.index(indexCurrent, offsetBy: -1)
-                charExpect = s[indexExpect]
-                lengthCurrent = 1
             }
         }
         return stringPalindrome
