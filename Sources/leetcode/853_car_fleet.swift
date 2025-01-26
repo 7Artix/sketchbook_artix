ed96ef4146434238ca8d0ce1694a41cba646081c
Artix <artixzhang@icloud.com>
2025-01-26 20:49:47 +0800

    853

--- Sources/leetcode/853_car_fleet.swift
+++ Sources/leetcode/853_car_fleet.swift
@@ -2,6 +2,7 @@ class Solution_853 {
     static let target = 21
     static let position = [1,15,6,8,18,14,16,2,19,17,3,20,5]
     static let speed = [8,5,5,7,10,10,7,9,3,4,4,10,2]
+    
     static func carFleet(_ target: Int, _ position: [Int], _ speed: [Int]) -> Int {
         var cars = zip(position,speed).map {($0, $1)}
         cars = cars.sorted {$0.0 > $1.0}
